4397fca730b9603e7abe2c0e5e934dd064fd0ccd
45panov <khorkov.anton@gmail.com>
2022-07-25 14:59:41 +0300

    Empty date_time.json error fixed.

--- chronos.py
+++ chronos.py
@@ -3,80 +3,83 @@ import time
 import json
 
 
-default_user = 'tihonasiy'
+default_user = "tihonasiy"
 default_time = 7200
-date_time_file = os.path.expanduser('~') + '/chronos/date_time.json'
+date_time_file = os.path.expanduser("~") + "/chronos/date_time.json"
 
 
 def timer():
 
-	# Reads remaining time from date_time.json and decreases
-	# its value in each iterration.
+    # Reads remaining time from date_time.json and decreases
+    # its value in each iterration.
 
-    with open(date_time_file, mode='r', encoding='utf-8') as f:
+    with open(date_time_file, mode="r", encoding="utf-8") as f:
 
         json_entry = json.load(f)
 
-    
-    while json_entry['remaining_time'] > 0:
-        
-        with open(date_time_file, mode='w', encoding='utf-8') as f:
-            
-            json_entry['remaining_time'] -= 1
+    while json_entry["remaining_time"] > 0:
+
+        with open(date_time_file, mode="w", encoding="utf-8") as f:
+
+            json_entry["remaining_time"] -= 1
 
             json.dump(json_entry, f)
 
             f.flush()
 
             time.sleep(1)
-		
+
+
 def reset_date_and_timer():
 
-	# Updates current date in date_time.json
+    # Updates current date in date_time.json
 
-    with open(date_time_file, mode='w', encoding='utf-8') as f:
+    with open(date_time_file, mode="w", encoding="utf-8") as f:
 
-        json_entry = {'current_date': time.strftime('%Y%m%d', time.gmtime()),
-                      'remaining_time': default_time}
-    
+        json_entry = {
+            "current_date": time.strftime("%Y%m%d", time.gmtime()),
+            "remaining_time": default_time,
+        }
+
         json.dump(json_entry, f)
 
         f.flush()
 
 
-
 # Main part starts below.
 
 
-if os.path.exists(date_time_file) == False: # Creates file with date and time if it doesn't exist.
+if os.path.exists(date_time_file) == False or os.stat(date_time_file).st_size == 0:
+
+    # Creates file with date and time if it doesn't exist or empty.
 
     reset_date_and_timer()
-        
-    with open(date_time_file, mode='r', encoding='utf-8') as f:
+
+    with open(date_time_file, mode="r", encoding="utf-8") as f:
 
         json_entry = json.load(f)
 
 
+else:  # Reads data from date_time_file.
 
-else: #read data from date_time_file
-
-    with open(date_time_file, mode='r', encoding='utf-8') as f:
+    with open(date_time_file, mode="r", encoding="utf-8") as f:
 
         json_entry = json.load(f)
 
 
+if json_entry["current_date"] == time.strftime("%Y%m%d", time.gmtime()):
 
-if json_entry['current_date'] == time.strftime('%Y%m%d', time.gmtime()):  #compare last saved and current dates
+    # Compare last saved and current dates.
 
     timer()
 
-    os.system('pkill -KILL -u {0}'.format(default_user))
-    
+    os.system("pkill -KILL -u {0}".format(default_user))
+
 
 else:
 
     reset_date_and_timer()
 
     timer()
-    
-    os.system('pkill -KILL -u {0}'.format(default_user))
+
+    os.system("pkill -KILL -u {0}".format(default_user))
