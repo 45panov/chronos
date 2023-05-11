65782af741dff4f2e9897e2898bde0b5374f5609
45panov <khorkov.anton@gmail.com>
2023-05-11 14:41:42 +0300

    test_system_performs_logout fails

--- tests/test_system.py
+++ tests/test_system.py
@@ -2,4 +2,8 @@ from chronos import *
 
 def test_system_return_timer():
     timer = System().set_timer(1)
-    assert timer.remain == Timer(1).remain
+    assert timer.remain == Timer(1).remain, "System().set_timer() must return Timer object"
+
+def test_system_performs_logout():
+    assert System().logout() == "os.system(System().logout_command())"
+
