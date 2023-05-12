4f96e765986f98d0db2efd5ed5372f11481dca0e
45panov <khorkov.anton@gmail.com>
2023-05-12 17:33:53 +0300

    Finish job with broken test (c)

--- tests/test_system.py
+++ tests/test_system.py
@@ -1,6 +1,7 @@
 from chronos import *
 
 def test_system_return_timer():
+    j_data = JData()
     j_data = {'remaining_time': 1}
     timer = System().set_timer(j_data['remaining_time'])
     assert timer.remain == Timer(1).remain, "System().set_timer() must return Timer object"
