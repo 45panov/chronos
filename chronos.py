89825c4052ffcac24cc86d2418c498d8c4b19213
45panov <khorkov.anton@gmail.com>
2023-05-22 17:33:54 +0300

    Different paths to cjdata on POSIX and NT

--- chronos.py
+++ chronos.py
@@ -5,14 +5,20 @@ class System:
     LOGOUT_COMMANDS = {'posix': f"pkill -kill -u {os.getlogin()}",  # Keeps logout commands for different OS
                        'nt': "shutdown -l"}
     
-
-    def set_timer(self, seconds):
-        return Timer(seconds)
+    PATH_TO_CJDATA = {'posix': "/etc/cjdata.json", # Keeps paths to cjdata.json for different OS
+                      'nt': "$TMP/cjdata.json"}
 
     @classmethod
     def logout(cls):
         return "os.system(" + cls.LOGOUT_COMMANDS[os.name] + ")"
        
+    @classmethod
+    def path_to_cjdata(cls):
+        return cls.PATH_TO_CJDATA[os.name]
+
+    def set_timer(self, seconds):
+        return Timer(seconds)
+
 
 class Timer:
     def __init__(self, seconds: int):
@@ -28,7 +34,8 @@ class JData():
             with open("/tmp/cjdata.json", mode='w') as f:
                 self.time_remain = 10
                 json.dump(self.time_remain, f)
-            
+
+
 class Chronos:
     def run(timer: Timer):
         timer.is_run = True
--- tests/test_system.py
+++ tests/test_system.py
@@ -1,4 +1,5 @@
 from chronos import *
+import pytest
 
 def test_system_return_timer():
     jdata = JData()
@@ -8,5 +9,8 @@ def test_system_return_timer():
 def test_system_performs_logout():
     assert System.logout() == "os.system(" + System().LOGOUT_COMMANDS[os.name] + ")"
 
-
-
+@pytest.mark.parametrize('test_os, expected', [('posix', "/etc/cjdata.json"), 
+                                               ('nt', "$TMP/cjdata.json")])
+def test_path_to_cjdata(test_os, expected):
+    if os.name == test_os:
+        assert System.path_to_cjdata() == expected 
