e7021aaac20c012e3f46cbf8f9250d1f85fa2d5f
Nikita Khismatov <nikita.khismatov@playrix.com>
2023-07-18 10:16:25 +0400

    black formatting

--- chronos.py
+++ chronos.py
@@ -5,29 +5,29 @@ from sys import exit
 
 PRODUCTION = 0  # If set to 1 Chronos will perform former logout command.
 
-USER = 'afanasiy'  # Account for which Chronos should perform logout command.
+USER = "afanasiy"  # Account for which Chronos should perform logout command.
 
 DEFAULT_TIME = 5800  # Time in seconds pass before Chronos perform logout.
 
-STORAGE = os.path.expanduser('~') + '/chronos/storage.json'
+STORAGE = os.path.expanduser("~") + "/chronos/storage.json"
 
 CURRENT_DATE = time.strftime("%d%m%Y", time.localtime())
 
 LOGOUT_COMMANDS = {"posix": "pkill -kill -u " + USER, "nt": "shutdown -l"}
 
 
-class Storage():
+class Storage:
     def __init__(self):
         if not os.path.exists(STORAGE) or os.stat(STORAGE).st_size == 0:
             self.time_remain, self.last_date = DEFAULT_TIME, CURRENT_DATE
             self.save(self.time_remain, self.last_date)
         else:
-            with open(STORAGE, mode='r', encoding='utf-8') as f:
+            with open(STORAGE, mode="r", encoding="utf-8") as f:
                 self.time_remain, self.last_date = json.load(f).values()
 
     def save(self, time_value: int, date_value: str) -> None:
         """Takes time and date values and loads it to storage.json"""
-        with open(STORAGE, mode='w', encoding='utf-8') as f:
+        with open(STORAGE, mode="w", encoding="utf-8") as f:
             json.dump({"time_remain": time_value, "last_date": date_value}, f)
             f.flush()
 
@@ -58,6 +58,6 @@ timer = Timer(storage)
 while timer:
     timer.run()
 if not PRODUCTION and not timer:
-    print('Chronos finished its job in test mode')
+    print("Chronos finished its job in test mode")
     exit()
 os.system(f"{LOGOUT_COMMANDS[os.name]}")
